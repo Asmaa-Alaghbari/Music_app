ff7873523524caaa318577003d3dde58417fc611
Asmaa Alaghbari <73389316+Asmaa-Alaghbari@users.noreply.github.com>
2023-08-09 12:38:28 +0300

    init commit

---
+++ music-app/src/App.js
@@ -0,0 +1,22 @@
+// App.js is the main file of the project
+
+import React from "react";
+import Home from "./Components/Home/Home";
+import Playlist from "./Components/Playlist/Playlist";
+import Sidenav from "./Components/Sidenav/Sidenav";
+import MusicTab from "./Components/MusicTab/MusicTab";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <Home /> */}
+      {/* <Sidenav /> */}
+      {/* <Playlist /> */}
+      {/* <MusicTab /> */}
+    </>
+  );
+}
+
+export default App;
