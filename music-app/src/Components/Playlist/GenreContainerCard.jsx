8d384ed104d40e37134bda9341d3407ee03c4cf7
Maria Doinaru <maria2naru@gmail.com>
2023-08-23 14:11:50 +0300

    commit

--- music-app/src/Components/Playlist/GenreContainerCard.jsx
+++ music-app/src/Components/Playlist/GenreContainerCard.jsx
@@ -2,7 +2,7 @@ import "./HomePlaylist.css";
 
 export default function CardGenre() {
   return (
-    <div className="card-genre">
+    <div className="card">
       <h4 className="title">Hip Hop</h4>
     </div>
   );
