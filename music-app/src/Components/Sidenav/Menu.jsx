2dd113295094167db35f1ea47f1f18ef1c0964cf
Asmaa Alaghbari <73389316+Asmaa-Alaghbari@users.noreply.github.com>
2023-08-31 23:25:44 +0300

    Sidenav

--- music-app/src/Components/Sidenav/Menu.jsx
+++ music-app/src/Components/Sidenav/Menu.jsx
@@ -1,61 +1,77 @@
 // Menu.jsx
 
-import React from "react";
+import React, { useState } from "react";
 import { TiHome } from "react-icons/ti"; // Home icon
 import { GiMusicalScore } from "react-icons/gi"; // Genres icon
 import { IoIosAlbums } from "react-icons/io"; // Albums icon
 import { BsPersonFill } from "react-icons/bs"; // Artists icon
-import { Link } from "react-router-dom"; // Open Playlists Window page
+import { NavLink } from "react-router-dom"; // Open Playlists Window page
 import PlaylistsWindow from "../Playlist/PlaylistsWindow"; // Playlists Window page
 import GenresWindow from "../Playlist/GenresWindow"; // Genres Window page
 
 import "./Menu.css"; // Stylesheet
 
 export default function Menu() {
+  const [activeLink, setActiveLink] = useState(""); // State to track active link
+
+  // Handle click on link and set active link
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
   return (
     <div className="menu">
       <h3 className="menu-lists-header">Menu</h3>
       <ul>
         {/* Home page */}
         <li>
-          <TiHome className="icons-style" />{" "}
-          {
-            <Link to="/" className="menu-lists">
-              Explore
-            </Link>
-          }
+          <NavLink
+            exact // To make the link active only on exact match
+            to="/"
+            className={`menu-lists ${activeLink === "explore" ? "active" : ""}`}
+            onClick={() => handleLinkClick("explore")}
+          >
+            <TiHome className="icons-style" />
+            Explore
+          </NavLink>
         </li>
 
         {/* Show all Genres */}
         <li>
-          <GiMusicalScore className="icons-style" />{" "}
-          {
-            <Link
-              to="/genres-window"
-              component={GenresWindow}
-              className="menu-lists"
-            >
-              Genres
-            </Link>
-          }
+          <NavLink
+            to="/genres-window"
+            component={GenresWindow}
+            className={`menu-lists ${activeLink === "genres" ? "active" : ""}`}
+            onClick={() => handleLinkClick("genres")}
+          >
+            <GiMusicalScore className="icons-style" />
+            Genres
+          </NavLink>
         </li>
 
         {/* Show all Albums */}
         <li>
-          <IoIosAlbums className="icons-style" />{" "}
-          {
-            <Link
-              to="/playlists-window"
-              component={PlaylistsWindow}
-              className="menu-lists"
-            >
-              Albums
-            </Link>
-          }
+          <NavLink
+            to="/playlists-window"
+            component={PlaylistsWindow}
+            className={`menu-lists ${
+              activeLink === "playlists" ? "active" : ""
+            }`}
+            onClick={() => handleLinkClick("playlists")}
+          >
+            <IoIosAlbums className="icons-style" />
+            Albums
+          </NavLink>
         </li>
         <li>
-          <BsPersonFill className="icons-style" />{" "}
-          <span className="menu-lists">Artists</span>
+          <NavLink
+            to="/artists"
+            className={`menu-lists ${activeLink === "artists" ? "active" : ""}`}
+            onClick={() => handleLinkClick("artists")}
+          >
+            <BsPersonFill className="icons-style" />
+            Artists
+          </NavLink>
         </li>
       </ul>
     </div>
